f6de7d316d61330d6e7f083349782cbd70631067
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-25 12:47:40 +0530

    Create ArraySort.java

---
+++ ArraySort.java
@@ -0,0 +1,25 @@
+/* Sort() Equals() copyOf() */
+
+// Sort()
+
+import java.util.Arrays;
+import java.util.Scanner;
+class A 
+{
+    public static void main(String[] args) {
+         
+        int a[]=new int[5];
+        Scanner r=new Scanner(System.in);
+        System.out.print("Enter Data in Array ");
+        for(int i=0;i<a.length;i++)
+        {
+            a[i]=r.nextInt();
+        }
+        Arrays.sort(a);
+        for(int i=0;i<a.length;i++)
+        {
+            System.out.print(a[i]+" ");
+        }
+
+    }
+}
