be13d4213a1eb1bae3f6b445b78a64d47770d695
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-25 10:52:03 +0530

    Create ArraySum.java

---
+++ ArraySum.java
@@ -0,0 +1,22 @@
+/* Find Sum of Array Elements */
+import java.util.Scanner;
+class A 
+{
+    public static void main(String[] args) {
+        
+        int a[]=new int[5]; int sum=0;
+        Scanner r=new Scanner(System.in);
+        System.out.print("Enter Elements in Array ");
+        for(int i=0;i<a.length;i++)
+        {
+            a[i]=r.nextInt();
+        }
+        System.out.print("Array Elements ");
+        for(int i=0;i<a.length;i++)
+        {
+            System.out.print(a[i]+" ");
+            sum=a[i]+sum;
+        }
+        System.out.print("\nAddition of Array Elements "+sum)
+;    }
+}
